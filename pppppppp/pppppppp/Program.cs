655ddee296135effa19cea7558bea2c8444b15e4
zuhaq21 <57126201+zuhaq21@users.noreply.github.com>
2019-11-01 23:37:16 +0500

    Add files via upload

---
+++ pppppppp/pppppppp/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace pppppppp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("D:/Uni/BSE-5/VP/Assignments/Students.txt", String.Empty);
+            Student obj = new Student();
+            char exitChoice;
+            String searchId;
+            String delId;
+            String searchName;
+            int choice;
+            do
+            {
+                Console.Clear();
+                String name, semester, cgpa, department, university, id;
+                Console.WriteLine("Press 1 to add student.");
+                Console.WriteLine("Press 2 to search for student. ");
+                Console.WriteLine("Press 3 to delete a record of a student. ");
+                Console.WriteLine("Press 4 to check top 3 students.");
+                Console.WriteLine("Press 5 to mark attendence.");
+                Console.WriteLine("Press 6 to view attendence");
+                Console.WriteLine("\t\t\t Enter your choice  ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1) //add Student
+                {
+                    Console.Clear();
+                    Console.Write("Enter the Name Of Student:      ");
+                    name = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter the semester of student:  ");
+                    semester = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter the cgpa of student:      ");
+                    cgpa = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter the department of student: ");
+                    department = Convert.ToString(Console.ReadLine());
+                    Console.Write("Enter the university of student: ");
+                    university = Convert.ToString(Console.ReadLine());
+                    id = obj.setId();
+                    obj.addStudent(name, semester, cgpa, department, university, id);
+
+                }
+                else if (choice == 2)  // search Student
+                {
+                    Console.Clear();
+                    int searchChoice;
+                    Console.Write("press 1 for search by id.\n");
+                    Console.Write("press 2 for search by name.\n");
+                    Console.Write("enter choice\n");
+                    searchChoice = Convert.ToInt32(Console.ReadLine());
+                    if (searchChoice == 1)  // search by id
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Enter Id of Student.");
+                        searchId = Convert.ToString(Console.ReadLine());
+                        obj.searchById(searchId);
+                    }
+                    else if (searchChoice == 2)  // search by name
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Enter The Name Of Student");
+                        searchName = Convert.ToString(Console.ReadLine());
+                        obj.searchByName(searchName);
+                    }
+                    else   //wrong input
+                    {
+                        Console.WriteLine("Wrong Input");
+                        break;
+                    }
+                }
+                else if (choice == 3) //delete
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter id of Student.");
+                    delId = Convert.ToString(Console.ReadLine());
+                    obj.deleteStudent(delId);
+
+                }
+                else if (choice == 4)  // top 3
+                {
+                    Console.Clear();
+                    obj.displayTopThree();
+                }
+                else if (choice == 5) //mark attendence
+                {
+                    Console.Clear();
+                    Console.WriteLine("Write P for present and A for Absent");
+                    obj.markAttendence();
+                }
+                else if (choice == 6)  // view attendence;
+                {
+                    Console.Clear();
+                    Console.WriteLine("\t\t\tList of Students");
+                    obj.viewAttendence();
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine("Want to do again: y/n");
+                exitChoice = Convert.ToChar(Console.ReadLine());
+            }
+            while (exitChoice == 'y' || exitChoice == 'Y');
+            Console.WriteLine("press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pppppppp/pppppppp/Student.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pppppppp
+{
+    class Student
+    {
+        public void addStudent(String name, String semester, String cgpa, String department, String university, String id)
+        {
+            using (StreamWriter writetext = File.AppendText("D:/Uni/BSE-5/VP/Assignments/Students.txt"))
+            {
+                String attendence = "A";
+                writetext.WriteLine(id);
+                writetext.WriteLine(name);
+                writetext.WriteLine(semester);
+                writetext.WriteLine(cgpa);
+                writetext.WriteLine(department);
+                writetext.WriteLine(university);
+                writetext.WriteLine(attendence);
+            }
+        }
+
+        public string setId()
+        {
+            byte[] buffer = Guid.NewGuid().ToByteArray();
+            var FormNumber = BitConverter.ToUInt32(buffer, 0) ^ BitConverter.ToUInt32(buffer, 4) ^ BitConverter.ToUInt32(buffer, 8) ^ BitConverter.ToUInt32(buffer, 12);
+            return FormNumber.ToString("X");
+        }
+
+        public void deleteStudent(String id)
+        {
+            String[] arrText;
+            List<String> newText = new List<String>();
+            int found = 0;
+            arrText = File.ReadAllLines("D:/Uni/BSE-5/VP/Assignments/Students.txt");
+            for (int i = 0; i < arrText.Length; i = i + 7)
+            {
+                if (arrText[i] == id)
+                {
+                    found++;
+                    continue;
+                }
+                else
+                {
+                    newText.Add(arrText[i]);
+                    newText.Add(arrText[i + 1]);
+                    newText.Add(arrText[i + 2]);
+                    newText.Add(arrText[i + 3]);
+                    newText.Add(arrText[i + 4]);
+                    newText.Add(arrText[i + 5]);
+                    newText.Add(arrText[i + 6]);
+                }
+            }
+            if (found > 0)
+            {
+                Console.WriteLine("Record Deleted");
+            }
+            else
+            {
+                Console.WriteLine("Record not Found");
+            }
+            using (StreamWriter writetext = new StreamWriter("D:/Uni/BSE-5/VP/Assignments/Students.txt"))
+            {
+                for (int i = 0; i < arrText.Length - 7; i++)
+                {
+                    writetext.WriteLine(newText[i]);
+                }
+            }
+        }
+        public void searchById(String id)
+        {
+            string[] arrText;
+            int found = 0;
+            string[] studentData = new string[6];
+            arrText = File.ReadAllLines("D:/Uni/BSE-5/VP/Assignments/Students.txt");
+            for (int i = 0; i < arrText.Length; i = i + 7)
+            {
+                if (arrText[i] == id)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Id of Student is:          " + arrText[i]);
+                    Console.WriteLine("Name of Student is:        " + arrText[i + 1]);
+                    Console.WriteLine("Semester of Student is:    " + arrText[i + 2]);
+                    Console.WriteLine("CGPA of Student is:        " + arrText[i + 3]);
+                    Console.WriteLine("Department of Student is:  " + arrText[i + 4]);
+                    Console.WriteLine("University of Student is:  " + arrText[i + 5]);
+                    found++;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (found == 0)
+            {
+                Console.WriteLine("Student Not found by this Id.");
+            }
+        }
+        public void searchByName(String Name)
+        {
+            string[] arrText;
+            int found = 0;
+            arrText = File.ReadAllLines("D:/Uni/BSE-5/VP/Assignments/Students.txt");
+            Console.Clear();
+            for (int i = 1; i < arrText.Length; i = i + 7)
+            {
+                if (arrText[i] == Name)
+                {
+                    Console.WriteLine("Id of Student is:          " + arrText[i - 1]);
+                    Console.WriteLine("Name of Student is:        " + arrText[i]);
+                    Console.WriteLine("Semester of Student is:    " + arrText[i + 1]);
+                    Console.WriteLine("CGPA of Student is:        " + arrText[i + 2]);
+                    Console.WriteLine("Department of Student is:  " + arrText[i + 3]);
+                    Console.WriteLine("University of Student is:  " + arrText[i + 4]);
+                    found++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            if (found == 0)
+            {
+                Console.WriteLine("Student not founnd by This Name");
+            }
+        }
+        public void displayTopThree()
+        {
+            String name1="";
+            String id1="";
+            String Cgpa1="";
+            String name2="";
+            String id2="";
+            String Cgpa2="";
+            String name3="";
+            String id3="";
+            String Cgpa3="";
+            double max = 0;
+            double max1 = 0;
+            double max2 = 0;
+            double max3 = 0;
+            int lengthOfArray = 0;
+            string[] arrText;
+            arrText = File.ReadAllLines("D:/Uni/BSE-5/VP/Assignments/Students.txt");
+  
+            for (int i = 3; i < arrText.Length; i = i + 7)
+            {
+                max = Convert.ToDouble(arrText[i]);
+                if (max > max1)
+                {
+                    max2 = max1;
+                    name2 = name1;
+                    id2 = id1;
+                    max3 = max2;
+                    name3 = name2;
+                    id3 = id2;
+                    max1 = max;
+                    id1 = arrText[i - 3];
+                    name1 = arrText[i - 2];
+                }
+                else if(max > max2)
+                {
+                    max3 = max2;
+                    name3 = name2;
+                    id3 = id2;
+                    max2 = max;
+                    id2 = arrText[i - 3];
+                    name2 = arrText[i - 2];
+                }
+                else if (max > max3)
+                {
+                    max3 = max;
+                    id3 = arrText[i - 3];
+                    name3 = arrText[i - 2];
+                }
+            }
+            Console.WriteLine("\t\t\t\t First Position");
+            Console.WriteLine("Name of Sudent is :       " + name1);
+            Console.WriteLine("Id of Sudent is :         " + id1);
+            Console.WriteLine("CGPA of Sudent            " + max1);
+
+            Console.WriteLine("\n\t\t\t\t Second Position");
+            Console.WriteLine("Name of Sudent is :       " + name2);
+            Console.WriteLine("Id of Sudent is :         " + id2);
+            Console.WriteLine("CGPA of Sudent is :       " + max2);
+
+            Console.WriteLine("\n\t\t\t\tThird Position");
+            Console.WriteLine("Name of Sudent is :       " + name3);
+            Console.WriteLine("Id of Sudent is :         " + id3);
+            Console.WriteLine("CGPA of Sudent is :       " + max3);
+        }
+        public void markAttendence()
+        {
+            String[] arrText;
+            String attendence;
+            arrText = File.ReadAllLines("D:/Uni/BSE-5/VP/Assignments/Students.txt");
+            for (int i = 0; i < arrText.Length; i = i + 7)
+            {
+                Console.WriteLine(arrText[i] + "\n" + arrText[i + 1]);
+                attendence = Convert.ToString(Console.ReadLine());
+                arrText[i + 6] = attendence;
+            }
+            using (StreamWriter writetext = new StreamWriter("D:/Uni/BSE-5/VP/Assignments/Students.txt"))
+            {
+                for (int i = 0; i < arrText.Length; i++)
+                {
+                    writetext.WriteLine(arrText[i]);
+                }
+            }
+        }
+        public void viewAttendence()
+        {
+            string[] arrText;
+            arrText = File.ReadAllLines("D:/Uni/BSE-5/VP/Assignments/Students.txt");
+            Console.WriteLine("array Length: " + arrText.Length);
+            for (int i = 0; i < arrText.Length; i = i + 7)
+            {
+                Console.WriteLine("Id of Student is:          " + arrText[i]);
+                Console.WriteLine("Name of Student is:        " + arrText[i + 1]);
+                Console.WriteLine("Attendence:                " + arrText[i + 6]);
+            }
+        }
+    }
+}
